253ddeef1c3db99e7c23252893d92d023d0dc839
baylatSeck <baylatseck05@gmail.com>
2025-12-24 20:49:47 +0100

    feat: add basics products grid
    What: Render a simple products grid from a static product list
    
    Why: Establish the first working UI before adding cart interactions

---
+++ data/products.js
@@ -0,0 +1,5 @@
+export const products = [
+  { id: 'p1', name: 'Black Socks', priceCents: 1090 },
+  { id: 'p2', name: 'Basketball', priceCents: 2095 },
+  { id: 'p3', name: 'T-Shirt Pack', priceCents: 799 }
+];
